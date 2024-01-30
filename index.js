fe1c4368c3ced74c55e8652aa946dc7335666891
kidus7 <kidusanteneh7@gmail.com>
2024-01-31 00:47:33 +0300

    small fix

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ module.exports = async (req, res) => {
   if (req.method !== 'GET') {
     res.status(405).send('Method Not Allowed');
     return;
-
+  }
     res.status(200).json({message: "This is a Footbal matches scores fetching API" });
-}
 };
